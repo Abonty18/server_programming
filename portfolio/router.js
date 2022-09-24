68e47ef43055e5fe6d18edb171fa0dad27552595
Abonty18 <labibafarah2998@gmail.com>
2022-09-25 03:24:03 +0600

    bug fixed

--- portfolio/router.js
+++ portfolio/router.js
@@ -25,8 +25,28 @@ router.post("/form",function(req,res){
     //     }
     // });
 
+    educations=fs.readFileSync("data/education",{encoding:"utf-8"});
+     educations=JSON.parse(String(educations));
+
+     experience=fs.readFileSync("data/experience",{encoding:"utf-8"});
+     experience=JSON.parse(String(experience));
+
+     {edus=[];
+     for(let key in educations)
+     {
+          edus.push(educations[key]);
+     }
+
+     
+
+     exp=[];
+     for(let key in experience)
+     {
+          exp.push(experience[key]);
+     }}
+
     console.log(f);
-    res.render("cv", { name: username,email:email, educations: edus ,});
+    res.render("cv", { name: username,email:email,educations:edus,experience:exp});
 });
 
 module.exports = router;
